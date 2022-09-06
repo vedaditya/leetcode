a5b624786e1cc84e7b46fd8a80d325b73c0d964d
vedaditya <41949512+vedaditya@users.noreply.github.com>
2022-09-06 22:54:29 +0530

    Time: 63 ms (17.11%), Space: 13.9 MB (68.92%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -10,20 +10,27 @@ class Solution:
             return None
 
         left,right=self.pruneTree(root.left),self.pruneTree(root.right)
-        if left and right:
+        root.left,root.right=None, None 
+        if left:
             root.left=left
-            root.right=right
-            return root
-        elif left:
-            root.right=None
-            root.left=left
-            return root
-        elif right:
-            root.left=None
-            root.right=right
-            return root
-        else:
-            root.left=None
-            root.right=None
-            return root if root.val==1 else None 
+        if right:
+            root.right =right 
+        return root if (left or right or root.val) else None 
+        
+        # if left and right:
+        #     root.left=left
+        #     root.right=right
+        #     return root
+        # elif left:
+        #     root.right=None
+        #     root.left=left
+        #     return root
+        # elif right:
+        #     root.left=None
+        #     root.right=right
+        #     return root
+        # else:
+        #     root.left=None
+        #     root.right=None
+        #     return root if root.val==1 else None 
         
